0e62b81e6b7764ad8afcdb9f3b7f42162c181d69
Agnė Rauluševičiūtė <agne.rauluseviciute@ktu.edu>
2023-12-09 18:54:30 +0200

    app.js

--- package/src/App.js
+++ package/src/App.js
@@ -1,10 +1,27 @@
-import { useRoutes } from "react-router-dom";
+// import { useRoutes } from "react-router-dom";
+// import Themeroutes from "./routes/Router";
+
+// const App = () => {
+//   const routing = useRoutes(Themeroutes);
+
+//   return <div className="dark">{routing}</div>;
+// };
+
+// export default App;
+
+import { HashRouter as Router, useRoutes } from "react-router-dom";
 import Themeroutes from "./routes/Router";
 
 const App = () => {
   const routing = useRoutes(Themeroutes);
 
-  return <div className="dark">{routing}</div>;
+  return (
+    <div className="dark">
+      <Router>
+        {routing}
+      </Router>
+    </div>
+  );
 };
 
 export default App;
